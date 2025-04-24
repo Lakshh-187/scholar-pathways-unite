614c9782c687be3d810361a02d1a2cbf7599783c
lakshaymandhan9720 <lakshaymandhan9720@gmail.com>
2025-04-24 15:37:11 +0000

    ac

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    base:"/",
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name]-[hash].js`,
+          chunkFileNames: `assets/[name]-[hash].js`,
+          assetFileNames: `assets/[name]-[hash].[ext]`,
+        },
+      },
+    },
+  
+    
+    host: "::",    
+    
+    
     port: 8080,
   },
   plugins: [
